68c64ebb0b44804bb41fcdf7b859bffe783a8e2f
Fabián <fabian8aa@gmail.com>
2024-06-02 15:11:31 -0500

    Add Dockerfile

--- api/api.py
+++ api/api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import json
+import boto3
 from datetime import datetime
 
 app = Flask(__name__)
@@ -17,7 +18,22 @@ def collect_data():
 
     with open(filename,'w') as file_json:
          json.dump(data, file_json, indent=4)
+    upload_s3(filename,'os-inspector-data', filename)     
     return jsonify({"status": "success"}), 200
 
+@app.route('/health', methods=['GET'] )
+def validate_health():
+    return jsonify({"health": "ok"}), 200
+
+def upload_s3(file, bucket, identifier):
+    try:
+        s3.upload_file(file, bucket, identifier)
+        print("uploaded")
+    except FileNotFoundError:
+        print("file not found")
+    except NoCredentialsError:
+        print(" auth error")
+
 if __name__ == '__main__':
+    s3 = boto3.client('s3')
     app.run(host='0.0.0.0', port=5000)
